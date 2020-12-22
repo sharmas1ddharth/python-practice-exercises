44a2f1907141b8abd9f50cdf27e0b67e28a22388
siddharth sharma <ssiddharth408@gmail.com>
2020-12-22 22:08:18 +0530

    exercises

---
+++ solutions/solution_07.py
@@ -0,0 +1,5 @@
+filename = input()
+
+get_extension = filename.split('.')
+
+print(get_extension[-1])
---
+++ solutions/solution_08.py
@@ -0,0 +1,4 @@
+color_list = ["Red", "Green", "White", "Black"]
+first_color = color_list[0]
+last_color = color_list[-1]
+print(first_color, last_color)
